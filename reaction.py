9a91778300d2986072a29fac437b077c0ffad125
Uzui-jin <18361900860@163.com>
2025-04-13 00:24:18 +0800

    detecting the buttons

--- reaction.py
+++ reaction.py
@@ -1,9 +1,21 @@
 from gpiozero import LED, Button
 from time import sleep
 from random import uniform
+from gpiozero import Button
 
 led = LED(4)
 led.on()
 sleep(uniform(5, 10))
 led.off()
 
+
+
+left_button = Button(14)
+right_button = Button(15)
+
+
+def pressed(button):
+	print(str(button.pin.number) + ' won the game')
+
+right_button.when_pressed = pressed
+left_button.when_pressed = pressed
